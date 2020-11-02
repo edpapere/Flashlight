fa7736d1bed55d3a043394323ae218d1b1154048
Andrey Pereslavtsev <pereslavtsev@yandex.ru>
2020-11-02 09:37:40 +0300

    Initial version. Add button and methods to toggle light (screen) on/off.

--- Flashlight/ViewController.swift
+++ Flashlight/ViewController.swift
@@ -9,11 +9,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var button: UIButton!
+    var isLightOn = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        updateUI()
     }
 
-
+    fileprivate func updateUI() {
+        self.view.backgroundColor = isLightOn ? .white : .black
+    }
+    
+    @IBAction func buttonPressed() {
+        isLightOn.toggle()
+        updateUI()
+    }
+    
 }
 
