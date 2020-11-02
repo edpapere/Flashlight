1d8d3e952cb777063af9e9bc7896c51b8543c0f7
Andrey Pereslavtsev <pereslavtsev@yandex.ru>
2020-11-02 15:25:09 +0300

    Add luminosity adjustment by swiping up and down.

--- Flashlight/ViewController.swift
+++ Flashlight/ViewController.swift
@@ -14,18 +14,76 @@ class ViewController: UIViewController {
         true
     }
     
+    let minimumLuminosity: CGFloat = 0.2
+    var currentLuminosity: CGFloat = 1
+    var isAdjustmentMode = false
+
+    var lightColor: UIColor {
+        UIColor(red: currentLuminosity, green: currentLuminosity, blue: currentLuminosity, alpha: 1)
+    }
+    
+    var adjustmentScale: CGFloat {
+        self.view.frame.height / 2
+    }
+
+    // MARK: - METHODS
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         updateUI()
+        //print( #line, #function, adjustmentScale)
     }
 
     fileprivate func updateUI() {
-        self.view.backgroundColor = isLightOn ? .white : .black
+        self.view.backgroundColor = isLightOn ? lightColor : .black
     }
         
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        isLightOn.toggle()
+
+        // do not toggle the light in adjustment mode -- quit ajustment mode instead
+        if isAdjustmentMode {
+            isAdjustmentMode = false
+        } else {
+            isLightOn.toggle()
+        }
+
         updateUI()
+        //print(#line,#function, isLightOn, lightLuminosity)
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+        let touch = touches.first
+        if touch != nil {
+            
+            // Enter manual adjustment mode
+            isAdjustmentMode = true
+            
+            // if light is off then switch it on at minimal luminosity
+            if !isLightOn {
+                currentLuminosity = minimumLuminosity
+                isLightOn = true
+            }
+            
+            // Calculate adjustment change and apply to current luminosity
+            let change = touch!.previousLocation(in: nil).y - touch!.location(in: nil).y
+            currentLuminosity += ( change/adjustmentScale )
+            
+            // let luminosity to stay in interval [minimumLuminosity...1] and
+            // switch light off if luminosity was adjusted to lower than minimum
+            currentLuminosity = currentLuminosity > 1.0 ? 1.0 : currentLuminosity
+            if currentLuminosity < minimumLuminosity {
+                currentLuminosity = minimumLuminosity
+                isLightOn = false
+            }
+            
+            updateUI()
+            
+            //print(#line,#function, isLightOn, currentLuminosity, change)
+        }
+        
+        super.touchesMoved(touches, with: event)
+            
     }
     
 }
